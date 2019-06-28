7a31cc955fed349766ecdf4aa6d2d1e9db83aa54
larisarysbekova <user@email.com>
2019-06-28 20:18:27 +0600

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,28 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String[] wizN = new String[3];
-        wizN[0] = "Ринсвинд";
-        wizN[1] = "Макс";
-        wizN[2] = "Гарри";
+        String[] wizN = {"Ринсвинд","Макс","Гарри"};
 
-        wizN = new String[4];
-        wizN[0] = "Ринсвинд";
-        wizN[1] = "Макс";
-        wizN[2] = "Гарри";
+        wizN = Arrays.copyOf(wizN,wizN.length+1);
         wizN[3] = "Гэндальф";
 
         for (int w = 0; w < wizN.length; w++){
             switch (w){
                 case 0:
-                    System.out.println("Доброе утро "+ wizN[0]);
-                    break;
+                    System.out.println("Доброе утро "+ wizN[w]);
+                break;
                 case 1:
-                    System.out.println("Добрый день "+ wizN[1]);
+                    System.out.println("Добрый день "+ wizN[w]);
                     break;
                 case 2:
-                    System.out.println("Добрый вечер "+ wizN[2]);
+                    System.out.println("Добрый вечер "+ wizN[w]);
                     break;
                 case 3:
-                    System.out.println("Спокойной ночи "+ wizN[3]);
+                    System.out.println("Спокойной ночи "+ wizN[w]);
                     break;
             }
         }
