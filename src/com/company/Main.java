e73f9c26ecb27c22d03d694301f5ce06848b5a38
larisarysbekova <user@email.com>
2019-06-28 19:59:31 +0600

    Дополнительно

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String[] wizN = {"Ринсвинд", "Макс", "Гарри"};
+        String[] wizN = new String[3];
+        wizN[0] = "Ринсвинд";
+        wizN[1] = "Макс";
+        wizN[2] = "Гарри";
+
+        wizN = new String[4];
+        wizN[0] = "Ринсвинд";
+        wizN[1] = "Макс";
+        wizN[2] = "Гарри";
+        wizN[3] = "Гэндальф";
+
         for (int w = 0; w < wizN.length; w++){
             switch (w){
                 case 0:
@@ -14,6 +24,9 @@ public class Main {
                     break;
                 case 2:
                     System.out.println("Добрый вечер "+ wizN[2]);
+                    break;
+                case 3:
+                    System.out.println("Спокойной ночи "+ wizN[3]);
                     break;
             }
         }
